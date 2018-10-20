1e1c165e0f306c6e714722823b017cc65f852f62
Anna <anna.brakowska91@gmail.com>
2018-10-20 13:13:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//we are using const because this we are using common js modules module
+// es2015 modules supports import
+// import on the front end, const require on the back end
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
